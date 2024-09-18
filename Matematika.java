ee3b8a9f397571fad1ec90ff94e7bca1373f0588
RizkinaFatikhatusS <165800759+RizkinaFatkhatusS@users.noreply.github.com>
2024-09-19 00:38:41 +0700

    Add files via upload

---
+++ Matematika.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugaspraktikum_3;
+
+/**
+ *
+ * @author user
+ */
+public class Matematika {
+    public int pertambahan(int a, int b) {
+        return a + b;
+    }
+
+ 
+    public int pengurangan(int a, int b) {
+        return a - b;
+    }
+
+   
+    public int perkalian(int a, int b) {
+        return a * b;
+    }
+
+ 
+    public int pembagian(int a, int b) {
+       
+        return a / b;
+    }
+}
